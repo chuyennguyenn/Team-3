cc65d9468dbd41bcfbca56864865b39ce96f14fb
DJ-Addy <achermouh808@gmail.com>
2024-06-30 19:58:36 -0500

    spawner, fear, swarm mechanics

--- Team 3/Assets/Scripts/EnemyController.cs
+++ Team 3/Assets/Scripts/EnemyController.cs
@@ -6,44 +6,75 @@ public class EnemyController : MonoBehaviour
 {
     public float speed = 3f;
     private Transform target;
+    private float _health;
+    private float _maxHealth=4;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Start()
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            target = other.transform;
-
-            Debug.Log("Player entered the radius: " + target);
-        }
+        _health = _maxHealth;
     }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        // Check if the object exiting the trigger is tagged as "Player"
-        if (other.gameObject.CompareTag("Player"))
+    public float Health
         {
-            // Clear the target when the player exits the radius
-            target = null;
+            set
+            {
+                _health = value;
 
-            // Log a message indicating the player has exited the radius
-            Debug.Log("Player exited the radius");
-        }
-
-
+                if (_health <= 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
     }
 
-    private void Update()
-    {
-        // If there is a target, move towards it
-        if (target != null)
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            // Calculate the direction to the target
-            Vector2 direction = target.position - transform.position;
-            direction.Normalize();
+            if (other.gameObject.CompareTag("Player"))
+            {
+                target = other.transform;
 
-            // Move the enemy towards the target
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                Debug.Log("Player entered the radius: " + target);
+            }
         }
-    }
 
-}
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            // Check if the object exiting the trigger is tagged as "Player"
+            if (other.gameObject.CompareTag("Player"))
+            {
+                // Clear the target when the player exits the radius
+                target = null;
+
+                // Log a message indicating the player has exited the radius
+                Debug.Log("Player exited the radius");
+            }
+
+
+        }
+
+        private void Update()
+        {
+            // If there is a target, move towards it
+            if (target != null)
+            {
+                Vector2 direction = target.position - transform.position;
+                direction.Normalize();
+
+            if (_health < _maxHealth / 2)
+                 {
+                        // Move the enemy away from the target
+                    Vector2 oppositeDirection = (Vector2)transform.position - direction;
+                    transform.position = Vector2.MoveTowards(transform.position, oppositeDirection, speed * Time.deltaTime);
+                    }
+                else
+                     {
+                // Move the enemy towards the target
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+                }
+        
+            }
+
+        }
+
+  }
