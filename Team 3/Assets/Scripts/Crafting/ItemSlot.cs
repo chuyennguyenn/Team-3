1b26ffa411d56270045bd39afe308664824ff18d
Steven Mensah <mensahsteven21@gmail.com>
2024-07-16 14:18:06 -0500

    selectable items

--- Team 3/Assets/Scripts/Crafting/ItemSlot.cs
+++ Team 3/Assets/Scripts/Crafting/ItemSlot.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 // Code from Night Run Studio's Inventory System Tutorial Series. Modified by Steven Mensah.
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //===ITEM DATA===//
     public string itemName;
@@ -20,6 +21,16 @@ public class ItemSlot : MonoBehaviour
     [SerializeField]
     private Image itemImage;
 
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    private InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = GameObject.Find("Canvas").GetComponent<InventoryManager>();
+    }
+
     public void AddItem(string itemName, int quantity, Sprite itemSprite) 
     {
         this.itemName = itemName;
@@ -31,5 +42,29 @@ public class ItemSlot : MonoBehaviour
         quantityText.enabled = true;
         itemImage.sprite = itemSprite;
         itemImage.enabled = true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick()
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+
+    public void OnRightClick()
+    {
+        
     }
 }
