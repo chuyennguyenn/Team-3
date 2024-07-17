157aef97a6c97b7f59b78783fb5f758361c725e3
chuyennguyen <chtnguyen@knox.edu>
2024-07-17 01:53:32 -0500

    add puzzle, quicksand, cactus

--- Team 3/Assets/Scripts/Map/puzzlePiece.cs
+++ Team 3/Assets/Scripts/Map/puzzlePiece.cs
@@ -8,19 +8,46 @@ public class puzzlePiece : MonoBehaviour
 
     GameObject trig;
     Vector2 startPos;
+    Rigidbody2D thisBody;
 
     void Start()
     {
         trig = GameObject.Find("triggerPlace");
         startPos = transform.position;
+        thisBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+
     void Update()
     {
         if (trig.GetComponent<triggerPlace>().reset == true)
         {
-             this.transform.position = startPos;
+            this.transform.position = startPos;
+        }
+    }  
+             
+        
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(thisBody.mass);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            thisBody.mass = 30;
+            Debug.Log("dcm");
+        }
+        
+    }
+
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log(thisBody.mass);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            thisBody.mass = 10000;
+            Debug.Log("dcm");
         }
     }
 }
