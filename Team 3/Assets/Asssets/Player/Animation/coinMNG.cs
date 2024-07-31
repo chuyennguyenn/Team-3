d8100d54ce4e10c442ab702a02fbdbe630183640
DJ-Addy <achermouh808@gmail.com>
2024-07-30 22:14:35 -0500

    Revert "remade stuff"
    This reverts commit 826d2419b96dace0741bfc2b5704b939765fd959.

---
+++ Team 3/Assets/Asssets/Player/Animation/coinMNG.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class coinMNG : MonoBehaviour
+{   
+    public int coinCount;
+    public Text coinText;
+    public GameObject door;
+    private bool doorDestroyed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = "COIN: " + coinCount.ToString();
+        
+        if (coinCount == 6 && !doorDestroyed){
+            doorDestroyed = true;
+            Destroy(door);
+        }
+    }
+}
