52b76a686e82ee74a693a326a70fa7362119bfc1
DJ-Addy <achermouh808@gmail.com>
2024-07-31 00:00:17 -0500

    working animation*

--- Team 3/Assets/Asssets/Enemy/Boss/BossAnimator.cs
+++ Team 3/Assets/Asssets/Enemy/Boss/BossAnimator.cs
@@ -13,6 +13,23 @@ public class BossAnimator : MonoBehaviour
         originalScale = transform.localScale;
     }
 
+    public bool IsInState(string stateName)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    public void SetEntrance(bool entrance)
+    {
+        animator.SetBool("entrance", entrance);
+    }
+
+    public void SetMoving(bool isMoving)
+    {
+        animator.SetBool("isMoving", isMoving);
+    }
+
+
+
     public void RotateToPointer(Vector2 lookDirection)
     {
         Vector3 scale = originalScale;
@@ -32,4 +49,34 @@ public class BossAnimator : MonoBehaviour
         animator.SetBool("isMoving", movementInput.magnitude > 0);
 
     }
+
+    public void TriggerAttack(int attackType)
+    {
+        switch (attackType)
+        {
+            case 1:
+                animator.SetTrigger("Hit1");
+                break;
+            case 2:
+                animator.SetTrigger("Hit2");
+                break;
+            case 3:
+                animator.SetTrigger("Hit3");
+                break;
+            case 4:
+                animator.SetTrigger("Spatk");
+                break;
+        }
+    }
+
+    public void TriggerTakeHit()
+    {
+        animator.SetTrigger("TakeHit");
+    }
+
+    public void TriggerFrozen()
+    {
+        animator.SetBool("Frozen", true);
+    }
+
 }
--- Team 3/Assets/Asssets/Enemy/Boss/BossScript.cs
+++ Team 3/Assets/Asssets/Enemy/Boss/BossScript.cs
@@ -11,27 +11,53 @@ public class BossScript : MonoBehaviour
 
     public Vector2 PointerInput { get => pointerInput; set => pointerInput = value; }
     public Vector2 MovementInput { get => movementInput; set => movementInput = value; }
+    private bool isAlive = true;
 
     private void Update()
     {
+        if (!isAlive)
+        {
+            return;
+        }
         //pointerInput = GetPointerInput();
         //movementInput = movement.action.ReadValue<Vector2>().normalized;
 
         agentMover.MovementInput = MovementInput;
-        //weaponParent.PointerPosition = pointerInput;
+        
         AnimateCharacter();
+
+        if (agentAnimations.IsInState("Entrance"))
+        {
+            agentAnimations.SetEntrance(false);
+        }
     }
 
     public void PerformAttack()
     {
-        //weaponParent.Attack();
+        if (!isAlive) return;
+
+        // Randomly choose an attack
+        int attackType = Random.Range(1, 4); // Assuming 1, 2, 3, spatk as different attacks
+        agentAnimations.TriggerAttack(attackType);
     }
 
     private void Awake()
     {
         agentAnimations = GetComponentInChildren<BossAnimator>();
-        //weaponParent = GetComponentInChildren<WeaponParent>();
         agentMover = GetComponent<BossMover>();
+    }
+
+    public void TakeDamage()
+    {
+        if (!isAlive) return;
+
+        agentAnimations.TriggerTakeHit();
+    }
+    
+    public void Die() {
+    isAlive = false;
+    agentAnimations.TriggerFrozen();
+    // Additional death logic, like stopping movement, disabling collisions, etc.
     }
 
     private void AnimateCharacter()
@@ -39,5 +65,17 @@ public class BossScript : MonoBehaviour
         Vector2 lookDirection = pointerInput - (Vector2)transform.position;
         agentAnimations.RotateToPointer(lookDirection);
         agentAnimations.PlayAnimation(MovementInput);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet2"))
+        {
+            Die(); // Trigger frozen state and set alive to false
+        }
+        else
+        {
+            TakeDamage(); // Handle regular hit
+        }
     }
 }
