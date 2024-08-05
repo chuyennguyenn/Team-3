b7d5f71382bb9dfaf9b07c9bb52dc886b5332502
chuyennguyen <chtnguyen@knox.edu>
2024-08-05 13:00:38 -0500

    Revert "update map, add transition"
    This reverts commit b0ac0fcd590ff4ebecef637a23bb0bd58ed62375.

--- Team 3/Assets/Scripts/Map/bossDoor.cs
+++ Team 3/Assets/Scripts/Map/bossDoor.cs
@@ -8,6 +8,15 @@ public class bossDoor : MonoBehaviour
     public int sceneBuildIndex;
     private static GameObject sampleInstance;
 
+    void Awake()
+    {
+        if (sampleInstance != null)
+            Destroy(sampleInstance);
+
+        sampleInstance = gameObject;
+        DontDestroyOnLoad(this);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         print("ok");
@@ -17,8 +26,14 @@ public class bossDoor : MonoBehaviour
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
             
             GameObject spawnBoss = GameObject.Find("bossRoom");
-            GameObject player = GameObject.FindWithTag("Player");
-            player.transform.position = new Vector2(-67, 205);
+            GameObject player = GameObject.Find("Player 1");
+            Debug.Log(player.transform.position);
+            player.transform.position = spawnBoss.transform.position;
+            Debug.Log(spawnBoss.transform.position);
+            Debug.Log(player.transform.position);
+
+
+
         }
     }
 }
--- Team 3/Assets/Scripts/Map/cactus.cs
+++ Team 3/Assets/Scripts/Map/cactus.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class cactus : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            dummy.health -= 1; 
-            Debug.Log(dummy.health);
-        }
+        collision.gameObject.GetComponent<PlayerCtrl>().health -= 1;
+        Debug.Log(collision.gameObject.GetComponent<PlayerCtrl>().health);
     }
 }
